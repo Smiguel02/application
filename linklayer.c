8e71b4daf646fca30f3de247a9c92c6dfcd6852a
Smiguel02 <miguelsilva.silva2@gmail.com>
2022-04-19 03:49:30 +0100

    RR and REJ working

--- linklayer.c
+++ linklayer.c
@@ -10,6 +10,9 @@
 #define BBC1_T A_T ^ SET // Será usado o mesmo Address no llopen, tanto para Transmitter como Receiver
 #define BCC1_R A_T ^ UA
 
+#define RR	0x01
+#define REJ	0x05
+
 #define HEADER_SIZE 4
 #define SUP_SIZE 5
 
@@ -32,8 +35,11 @@ struct termios oldtio, newtio;
 linkLayer ll;
 int fd;
 int state = 0;
-int S;
-int R;
+int S=0;
+int R=32;
+
+int get_baud(int baud);
+
 
 void escrita(){
   printf("TIMEDOUT maninho\n");
@@ -62,7 +68,7 @@ char wait_for_answer(){
 	  {
 
 	  case 0:
-		return 0;
+		return -1;
 
 	  // receber informaçao
 	  case 1:
@@ -144,7 +150,7 @@ int llopen(linkLayer connectionParameters)
     }
 
     bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = B9600 | CS8 | CLOCAL | CREAD;
+    newtio.c_cflag = get_baud(connectionParameters.baudRate) | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
 
@@ -296,14 +302,23 @@ int llwrite(char *buf, int bufSize)
   char *input = malloc(sizeof(char) * (inputSize));
   char stuffed[MAX_PAYLOAD_SIZE*2];
   char bcc2 = 0;
-  char help;
+  char help=0, rr_aux=0, rej_aux=0;
 
 	printf("bufSize=%d\n", bufSize);
 
   input[0] = FLAG;
   input[1] = A_T;
-  input[2] = SET;   //atualizar S se receçao da resposta foi bem sucedida
+  input[2] = S;   //atualizar S se receçao da resposta foi bem sucedida
   input[3] = input[1]^input[2];
+
+
+	rr_aux=RR^R;
+	rej_aux=REJ^R;	//nao tenho a certeza mas lets go with this
+	printf("rr_aux=%d\n", rr_aux);
+	printf("rej_aux=%d\n", rej_aux);
+
+	S=S^2;
+	R=R^32;
 
 
   // BCC2 creation
@@ -353,13 +368,14 @@ int llwrite(char *buf, int bufSize)
 		  return -1;
 		  break;
 		}
-
-		k++;
+		if(help!=rej_aux){
+			k++;
+		}
 
 		res = write(fd, stuffed, stuffedSize+1);
 		printf("Escrevemos %d BYTES em llwrite\n", res);
 		help=wait_for_answer();
-		if(!help){
+		if(help!=rr_aux){
 			printf("TimedOut, ou mal recebida, sending again\n");
 			state=0;
 			break;
@@ -401,7 +417,6 @@ int llwrite(char *buf, int bufSize)
 int llread(char *packet)
 {
   int packetSize=0, res;
-  char input[MAX_PAYLOAD_SIZE * 2];
   char output[5];
   char bcc2=0;
   char aux;
@@ -409,27 +424,20 @@ int llread(char *packet)
 
   output[0] = FLAG;
   output[1] = A_T;
-  output[2] = UA;
-  output[3] = output[1]^output[2];
   output[4] = FLAG;
 
 	state=1;
 	aux=wait_for_answer();
 
-if(!aux){
+
+if(aux<0){
 	printf("Header not well received, wait transmission\n");
 	return 0;
 }
 
-//como ja recebemos e verificamos, podemos escrever artificialmente os valores de input
-	input[0]=FLAG;
-	input[1]=A_T;
-	input[2]=aux;
-	input[3]=input[1]^input[2];
-
+	R=(aux^2)<<4;
 
 	int j=0;
-
 
 //como ja lemos anteriormente o HEADER, ja so temos DATA e BCC2 ate FLAG
 //FLAG also fica lida, need to tirar BCC2 de packet
@@ -453,14 +461,20 @@ while(1){
 packetSize=j;
 //debugging code
 
-	//verificamos if input[i-2]==bcc2
   if(bcc2){
-	printf("Error in received data (BCC2), trying again\n");
+	printf("Error in received data (BCC2), sending REJ\n");
+	output[2] = REJ^R;
+	output[3]= output[1]^output[2];
+	printf("output[2]=%d\n", output[2]);
+	res=write(fd, output, 5);
 	return 0;
   }
   packet[packetSize-1]='\0';
   packet[packetSize-2]='\0';
 
+output[2]=RR^R;
+output[3]=output[1]^output[2];
+printf("output[2]=%d\n", output[2]);
 
 res=write(fd, output, 5);
 printf("%d bytes written back in llread\n", res);
@@ -473,24 +487,213 @@ return packetSize-2;
 
 // Closes previously opened connection; if showStatistics==TRUE, link layer should print statistics in the console on close
 int llclose(int showStatistics){
+	
+	char output[SUP_SIZE]={0}, ack[SUP_SIZE]={0};
+	int k=0, res=0;
+	char aux=0;
 
-/*
+
+	output[0]=FLAG;
+	output[1]=A_T;
+	output[2]=DISC;
+	output[3]=A_T^DISC;
+	output[4]=FLAG;
+
+	ack[0] = FLAG;
+	ack[1] = A_T;
+	ack[2] = UA;
+	ack[3] = BCC1_R;
+	ack[4] = FLAG;
+
+printf("Entramos em llclose\n");
 switch(ll.role){
 
-	char discard[SUP_SIZE]=0, ack[SUP_SIZE]=0;
+
 
 	case TRANSMITTER:
+	state=0;
+		
+		while (state != 3)
+	{
+	
+	  switch (state)
+	  {
+
+	  // escrever informaçao e verificar
+	  case 0:
+		printf("Estamos em state=0 pela %d vez\n", k);
+
+		if (k == ll.numTries)
+		{
+		  printf("Nao recebeste dados nenhuns amigo, problems de envio\n");
+		  return -1;
+		}
+
+		k++;
+		res = write(fd, output, SUP_SIZE);
+		printf("%d Bytes written\n", res);
+		aux=wait_for_answer();
+		if(aux==DISC){
+			printf("Recebemos DISC llopen\n");
+			state=1;
+			break;
+		}
+		printf("Wait for answer error\n");
+		break;
+
+	  // receber informaçao
+		case 1:
+			while(!read(fd, &aux, 1)){}
+
+			if(aux!=FLAG){
+				printf("Nao recebemos last FLAG, trying again\n");
+				state=0;
+				break;
+			}
+
+		state++;
+		break;
+	  
+	  	case 2:
+			printf("UA written back\n");
+			write(fd, ack, SUP_SIZE);
+			state++;
+
+		break;
+
+	}
+
+	}
 
 	break;
 
 	case RECEIVER:
 
-	break;
+	state=0;
+	while (state != 4)
+	{
+	  // Receiver
+	  switch (state)
+	  {
+		case 0:
+			state=1;
+			aux=wait_for_answer();
+			if(aux==DISC){
+				printf("Header well received\n");
+				state=1;
+				break;
+			}
+
+			printf("Header badly received\n");
+			state=0;//ou return 0, who knows honestly
+			break;
+			
+		case 1:
+		printf("ENtramos em state=1\n");
+			while(!read(fd, &aux, 1)){}
+		printf("saimos do read\n");
+			if(aux!=FLAG){
+				printf("Nao recebemos last FLAG\n");
+				state=0;	//ou return, who knows
+				break;
+			}
+
+			printf("%ld bytes Written back\n",write(fd, output, SUP_SIZE));
+			state++;
+
+		break;
+
+		case 2:
+		printf("state=2\n");
+			state=1;
+			aux=wait_for_answer();
+			if(aux==UA){
+				printf("Header well received\n");
+				state=3;
+				break;
+			}			
+			printf("Header badly received\n");
+			state=0;	//ou return 0, who knows honestly
+
+		break;
+
+		case 3:
+		printf("state=3\n");
+
+			while(!read(fd, &aux, 1)){}
+
+			if(aux!=FLAG){
+				printf("Nao recebemos last FLAG\n");
+				state=0;	//ou return, who knows
+				break;
+			}
+		state=4;
+		break;
+	  }
+
+}
+break;
+
+}
+
+//fecha ligaçao
+if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+
+	printf("Connection closed\n");
+	sleep(1);
+    close(fd);
+    sleep(1);
+    return 0;
 
 
 
 }
-*/
 
 
+int get_baud(int baud)
+{
+    switch (baud) {
+    case 9600:
+        return B9600;
+    case 19200:
+        return B19200;
+    case 38400:
+        return B38400;
+    case 57600:
+        return B57600;
+    case 115200:
+        return B115200;
+    case 230400:
+        return B230400;
+    case 460800:
+        return B460800;
+    case 500000:
+        return B500000;
+    case 576000:
+        return B576000;
+    case 921600:
+        return B921600;
+    case 1000000:
+        return B1000000;
+    case 1152000:
+        return B1152000;
+    case 1500000:
+        return B1500000;
+    case 2000000:
+        return B2000000;
+    case 2500000:
+        return B2500000;
+    case 3000000:
+        return B3000000;
+    case 3500000:
+        return B3500000;
+    case 4000000:
+        return B4000000;
+    default: 
+        return -1;
+    }
 }
